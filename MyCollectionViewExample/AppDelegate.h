5d8154e708351f92ffcf25e607be12eb5ae4173b
Milan Panchal <sam07it22@gmail.com>
2014-02-04 11:18:37 +0530

    Initial Commit

---
+++ MyCollectionViewExample/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MyCollectionViewExample
+//
+//  Created by Milan Kumar Panchal on 04/02/14.
+//  Copyright (c) 2014 Pantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MyCollectionViewExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyCollectionViewExample
+//
+//  Created by Milan Kumar Panchal on 04/02/14.
+//  Copyright (c) 2014 Pantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
